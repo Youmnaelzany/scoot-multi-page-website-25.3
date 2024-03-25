31f2c8e7b1785ed616b49ed0848efe75b5a07529
Yaya <youmnael38@gmail.com>
2024-03-25 02:21:35 +0200

    adding folder and files for the project

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+  ],
+  prefix: "",
+  theme: {
+
+    extend: {
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+}
